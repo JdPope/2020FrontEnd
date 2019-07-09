72499728e4d9dcf7752aa4e46401c86398cc2297
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-09 11:46:28 -0600

    fixing fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default class App extends Component {
+
+constructor(props){
+  super(props)
+  this.state = {
+
+    markers: []
+  }
 }
 
-export default App;
+componentDidMount(){
+  let url = 'https://lit-cliffs-51825.herokuapp.com/markers'
+  fetch(url)
+    .then(response => response.json())
+    .then(result => this.setState({markers: result}))
+
+}
+
+render(){
+  return(
+
+    <div>
+      <h1>Dog TEST For President 2020</h1>
+      <h2>let's get some markers{this.state.markers.id}{console.log(this.state.markers)}</h2>
+
+
+    </div>
+
+  )
+}
+
+
+
+
+
+
+
+}
