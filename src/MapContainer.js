f94ed2bc425c35f3812db79bf7a7d0af0c28bc4d
Jessica Pope <jessica.pope.co@gmail.com>
2019-07-10 07:58:25 -0600

    fixed api issue

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
@@ -7,21 +7,21 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+
   render() {
     return (
       <Map
         google={this.props.google}
         zoom={14}
         style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-        />
+        initialCenter={{ lat: 47.444, lng: -122.176}}
+        >
+        <Marker position={{ lat: 48.00, lng: -122.00}} />
+      </Map>
      );
    }
  }
 
  export default GoogleApiWrapper({
-   apiKey: 
+   apiKey: //*{your api key here}*
  })(MapContainer);
